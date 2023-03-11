e9425ece133f0a4d9136d2064afe00ffb8f3fb7a
sajjadcodes <sajjadcodes@gmail.com>
2023-03-12 00:26:26 +0500

    added plugin header info

---
+++ weather-app.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Weather Plugin
+ * Description: A weather plugin powered by Vue.js
+ * Author: Sajad Hussain
+ * Author URI: https://sajjadcodes.com
+ * Version: v 0.1
+ * text-domain: weahter-app
+ */
+
+ if ( ! exist() ) {
+
+ }
